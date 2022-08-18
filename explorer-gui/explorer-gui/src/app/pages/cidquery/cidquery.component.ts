039e3ca97a9d9fe0a962e6b175acf0a1f83cb185
xlfguang <2684695148@qq.com>
2022-08-18 15:26:49 +0800

    feat: CID查询功能

--- explorer-gui/explorer-gui/src/app/pages/cidquery/cidquery.component.ts
+++ explorer-gui/explorer-gui/src/app/pages/cidquery/cidquery.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { ContractPromise } from "@polkadot/api-contract";
+import metadata from "../../../assets/metadata.json";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-dashboard",
@@ -8,22 +11,54 @@ import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
 })
 export class CidQueryComponent implements OnInit, OnDestroy {
   cid: string;
-  searchLoad:boolean = false;
+  searchLoad: boolean = false;
+  contract: ContractPromise;
+  mainCid: string[] = [];
+  thisPdfCid: string = "";
+  childCidList: string[] = [];
 
+  constructor(private httpClient: HttpClient) {}
   ngOnDestroy(): void {
-    console.log(1);
-    
+    console.log(this.contract);
   }
   ngOnInit(): void {
-
+    console.log(1);
+    this.connect();
   }
-  search(): void {
-    // Strip whitespace from search text
-    console.log(this.cid);
-    if(this.cid.length > 0){
+  async search(): Promise<void> {
+    if (this.cid.length > 0) {
       this.searchLoad = true;
-    }else{
-      this.searchLoad = false;
+    } else {
+      return;
     }
+    const { gasRequired, storageDeposit, result, output } =
+      await this.contract.query.getPdfCid("", {}, [this.cid]);
+    if (result.isOk) {
+      this.mainCid = output.toHuman() as string[];
+      this.thisPdfCid = this.cid;
+      this.getChildCid(this.mainCid[0]);
+    } else {
+      console.error("Error", result.asErr);
+    }
+    this.searchLoad = false;
   }
+
+  async connect(): Promise<void> {
+    const wsProvider = new WsProvider("wss://rpc.greenchain.cc");
+    const api = (await ApiPromise.create({ provider: wsProvider })) as any;
+    const contract = new ContractPromise(
+      api,
+      metadata,
+      "5F3RW9aKFUWfh894wZn2rd3R6yNz6gVRnDLJ3YsDByP2keCz"
+    );
+    this.contract = contract;
+  }
+
+  getChildCid = (cid: string) => {
+    this.httpClient
+      .get(`http://1.116.126.237/ipfs/${cid}`)
+      .subscribe((res: any) => {
+        this.childCidList = res;
+      });
+  };
 }
